63ab181bb369168e4a23802500f161bbbd148077
Pascal Rivoire <117156701+justausercoding@users.noreply.github.com>
2024-07-21 13:59:28 +0200

    Add import statement for html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,31 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: 'developement',
-    entry: './src/index.js',
+    mode: "developement",
+    entry: "./src/index.js",
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
     // -- Assets --
     module: {
         rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-        },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
         ],
     },
     // -- Plugins --
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/template.html',
+            template: "./src/template.html",
         }),
     ],
 };
